09c65d0d66c237aa1faffaa45101b78b21155a6d
Edgard Alexandre <edgard-alexandre@bol.com.br>
2016-06-23 12:13:00 -0300

    Add files via upload

---
+++ VersaoBoa/add.php
@@ -0,0 +1,40 @@
+//clientes
+//ok
+<?php
+	require_once 'init.php';
+	include_once 'cliente.class.php';
+	
+	//pega os dados do formulário
+	
+	$name = isset($_POST['txtNome']) ? $_POST['txtNome'] : null;
+	$email = isset($_POST['txtEmail']) ? $_POST['txtEmail'] : null;
+	$dataCadastro = isset($_POST['txtData']) ? $_POST['txtData'] : null;
+
+	//validação simples se campos estão vazios
+	if(empty($name) || empty($email) || empty($dataCadastro))
+	{
+		echo "Campos devem ser preenchidos!!";
+		exit;
+	}
+	
+	//instancia objeto cliente
+	$cliente = new Cliente($name,$email,$dataCadastro);
+
+	// insere no BD
+	$PDO = db_connect();
+	$sql = "INSERT INTO clientes(nomeCliente,email,dataCadastro) VALUES (:name,:email,:dataCadastro)";
+	$stmt = $PDO->prepare($sql);
+	$stmt->bindParam(':name', $cliente->getNome());
+	$stmt->bindParam(':email', $cliente->getEmail());
+	$stmt->bindParam(':dataCadastro', $cliente->getDataCadastro());
+
+	if($stmt->execute())
+	{
+		header('Location: index.php');
+	}
+	else
+	{
+		echo "Erro ao cadastrar!!";
+		print_r($stmt->errorInfo());
+	}
+?>
---
+++ VersaoBoa/form-add.php
@@ -0,0 +1,43 @@
+//OK
+<?php
+	require 'init.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Envio de dados</title>
+		<meta charset="utf-8">
+		<script type="text/javascript" src="jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="jquery.maskedinput.js"></script>
+	</head>
+	<body>
+	  <div class="conteudo">
+		<form method="post" name="formCadastro" action="add.php" enctype="multipart/form-data">
+			<h3 id="cliente">Cadastro Clientes</h3>
+			<h3 id="cliente1">Cadastro Clientes</h3>
+			<table class="tabelaClientes" width="100%">
+				<tr>
+					<th width="18%"> Nome </th>
+					<td width="82%"><input type="text" name="txtNome"></td>
+				</tr>	
+				<tr>
+					<th> Email </th>
+					<td><input type="email" name="txtEmail"></td>
+				</tr>
+				<tr>
+					<th> Data Cadastro </th>
+					<td><input id="data" type="text" name="txtData"></td>
+                    <script language="text/javascript">
+		            		$("#data").mask("99/99/9999");
+		        	</script>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" name="btnEnviar" value="Cadastrar">
+						<input type="reset" name="btnLimpar" value="Limpar"></td>
+				</tr>
+			</table>
+		</form>
+	  </div>
+	</body>
+</html>
